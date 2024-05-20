0e5af5f06a649df9944f405bf7c962b7db900074
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-05-20 17:03:53 +0400

    starting the frontend

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // import and intialize express
 require("dotenv").config()
 var express = require('express');
+var cors = require('cors')
 var app = express();
+
+app.use(cors())
+app.use(express.json())
 
 app.use(function (req, res, next){
   var logMsg = `${req.ip} requested ${req.url}`
@@ -11,5 +15,8 @@ app.use(function (req, res, next){
 
 var lessonsRouter = require('./routes/lessons')
 app.use("/lessons", lessonsRouter)
+
+var ordersRouter = require('./routes/orders')
+app.use("/orders", ordersRouter)
 
 app.listen(8080)
