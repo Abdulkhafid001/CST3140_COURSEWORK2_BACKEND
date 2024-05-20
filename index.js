1b73edcd7635c8d4580714b79d93965ea54e0876
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-05-20 17:04:15 +0400

    starting

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // import and intialize express
-require("dotenv").config()
-var express = require('express');
-var cors = require('cors')
+require("dotenv").config();
+var express = require("express");
+var cors = require("cors");
 var app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(function (req, res, next){
-  var logMsg = `${req.ip} requested ${req.url}`
-  console.log(logMsg)
+app.use(function (req, res, next) {
+  var logMsg = `${req.ip} requested ${req.url}`;
+  console.log(logMsg);
   next();
-})
+});
 
-var lessonsRouter = require('./routes/lessons')
-app.use("/lessons", lessonsRouter)
+var lessonsRouter = require("./routes/lessons");
+app.use("/lessons", lessonsRouter);
 
-var ordersRouter = require('./routes/orders')
-app.use("/orders", ordersRouter)
+var ordersRouter = require("./routes/orders");
+app.use("/orders", ordersRouter);
 
-app.listen(8080)
+app.listen(8080);
